dce06a5705bfb400309f0c8044822a2841ab43f9
nlakshmi-29 <nandetilakshmi123@gmail.com>
2021-05-20 15:09:17 +0530

    hello

---
+++ C PROGRAMMING/addingtwonum.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter No x");
+    scanf("%d",&x);
+    printf("Enter No y");
+    scanf("%d",&y);
+    z=x+y;
+    printf("Sum = %d",z);
+    return 0;
+}
---
+++ C PROGRAMMING/newhello.C
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("hello Good Morning");
+    return 0;
+}
